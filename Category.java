629258357fe91c4eb507bb30f18f2144f5618573
Prajakta-Badade <69361359+Prajakta-Badade@users.noreply.github.com>
2022-11-19 20:48:24 +0530

    Update Category.java

--- Category.java
+++ Category.java
@@ -8,7 +8,7 @@ import javax.ejb.RemoveException;
 
 public class Category extends Vehicle implements NewInterface {
 public class Category extends Vehicle implements NewInterface {
-
+ String category;
     String category;
     int categoryid;
     final int finalfield = 1;
