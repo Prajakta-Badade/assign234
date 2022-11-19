454cc37d20ddaba60f62ef9a1f9734b62eb299a3
Prajakta-Badade <69361359+Prajakta-Badade@users.noreply.github.com>
2022-11-19 21:40:39 +0530

    Update Category.java

--- Category.java
+++ Category.java
@@ -12,7 +12,7 @@ public class Category extends Vehicle implements NewInterface {
 
     int categoryid;
     final int finalfield = 1;
-    int x, y;
+    int x, y,z;
 
 
     public String getCategory() {
