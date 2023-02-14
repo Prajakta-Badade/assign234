461a6784d04f497c3526c64ad945542dd53a8a18
Prajakta-Badade <69361359+Prajakta-Badade@users.noreply.github.com>
2023-02-14 20:00:25 +0530

    Update Category.java

--- Category.java
+++ Category.java
@@ -71,6 +71,7 @@ System.out.println("X and Y are different");
         System.out.println("X and are different");
         System.out.println("X and Y are different");
         System.out.println("X nd Y are different");
+        System.out.println("X nd Y are different");
 
         
                 System.out.println("X and Y are different");
